7d19a2217620af74dbaea9d34a1dc07db3145f4d
SkylerTrent <trent.skyler56@gmail.com>
2020-10-28 12:53:18 +0000

    Added checkout form views and templates

--- amaterasu_computers/settings.py
+++ amaterasu_computers/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+     # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'amaterasu_computers.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -65,8 +70,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]

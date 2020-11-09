4e36170afecc01ca82cf9a83f5cf2ac63cf400b4
SkylerTrent <trent.skyler56@gmail.com>
2020-11-09 14:56:44 +0000

    Added account register link to vote

--- poll/urls.py
+++ poll/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.view_poll, name='view_poll'),
+    path('', views.view_poll, name='poll'),
     path('add_vote/<poll_product_id>/', views.add_vote, name='add_vote'),
 ]
